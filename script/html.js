4f85549c5ee5cc109c28b310687588c1ce3f72f8
KAOZ <kelvinalvescosta@hotmail.com>
2023-01-10 02:10:41 -0300

    cabecalho corrigido

--- script/html.js
+++ script/html.js
@@ -1,17 +1,40 @@
 function Menu() {
     if (items.style.display == 'block'){
-        items.style.display = 'none'
-        Loud.style.position = 'static'
-        header.style.background = 'black'
-        header.style.padding = '20px'
-        menu.style.color = 'white'                        
+        items.style.display = 'none'  
+        Loud.style.transform = 'translateX(0%)'                     
+        header.style.backgroundColor = 'black'
+        menu.style.color = 'white'
+        flexnav.style.color = 'white'
+        loja.style.color = 'white'
+        book.style.color = 'white'
+        time.style.color = 'white'
+        sobre.style.color = 'white'
+        blog.style.color = 'white'
+        vagas.style.color = 'white'
+        carrinho.style.color = 'white'
+        clube.style.color = 'white'
     }else{
         items.style.display = 'block'
-        header.style.background = 'white' 
-        Loud.style.position = 'absolute'
-        Loud.style.left = '45%'
-        Loud.style.bottom = '90%'
+        Loud.style.transform = 'translateX(245%)'
+        header.style.backgroundColor = 'white'
         menu.style.color = 'black'
+        flexnav.style.color = 'black'
+        loja.style.color = 'black'
+        book.style.color = 'black'
+        time.style.color = 'black'
+        sobre.style.color = 'black'
+        blog.style.color = 'black'
+        vagas.style.color = 'black'
+        carrinho.style.color = 'black'
+        clube.style.color = 'black'
+    
+    }
+}
 
+function Tamanho() {
+    if (window.innerWidth >= 768){
+        items.style.display = 'block'
+    }else{
+        items.style.display = 'none'
     }
 }
